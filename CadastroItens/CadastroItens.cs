148938b7f5f80b4d137adefae6f6c91ef5d73bda
mrGois <ezequielgois@yahoo.com.br>
2025-04-16 15:01:56 -0300

    Adição dos codigos aos botões e criação da tela edição de item

--- CadastroItens/CadastroItens.cs
+++ CadastroItens/CadastroItens.cs
@@ -16,5 +16,51 @@ namespace achados_e_perdidos_senac.CadastroItens
         {
             InitializeComponent();
         }
+
+        private void btnSelecionarFoto_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Imagens|*.jpg;*.jpeg;*.png;",
+                Title = "Selecionar Foto"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBoxFoto.ImageLocation = openFileDialog.FileName;
+            }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            private void Form1_Load(object sender, EventArgs e)
+        {
+            dateTimePickerDataCadastro.Value = DateTime.Now;
+            dateTimePickerDataCadastro.Enabled = false; 
+        }
+
+            private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            string descricao = txtDescricao.Text;
+            string caminhoFoto = pictureBoxFoto.ImageLocation;
+            DateTime dataCadastro = dateTimePickerDataCadastro.Value;
+
+            if (string.IsNullOrEmpty(descricao) || string.IsNullOrEmpty(caminhoFoto))
+            {
+                MessageBox.Show("Por favor, preencha todos os campos e selecione uma foto.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Aqui você pode salvar os dados em um banco de dados, arquivo ou listaMessageBox.Show("Item cadastrado com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            // Opcional: Limpar os campos após salvartxtDescricao.Clear();
+            pictureBoxFoto.ImageLocation = null;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
     }
 }
--- CadastroItens/EdicaoDeItens.Designer.cs
+++ CadastroItens/EdicaoDeItens.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace achados_e_perdidos_senac.CadastroItens
 {
-    partial class CadastroItens
+    partial class EdicaoDeItens
     {
         /// <summary>
         /// Required designer variable.
@@ -28,8 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CadastroItens));
-            this.panel1 = new System.Windows.Forms.Panel();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EdicaoDeItens));
             this.btnSalvar = new System.Windows.Forms.Button();
             this.btnSelecionarFoto = new System.Windows.Forms.Button();
             this.dateTimePickerDataCadastro = new System.Windows.Forms.DateTimePicker();
@@ -41,108 +40,86 @@
             this.panel4 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.panel1.SuspendLayout();
+            this.btnExcluir = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFoto)).BeginInit();
             this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
-            // 
-            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.panel1.Controls.Add(this.btnSalvar);
-            this.panel1.Controls.Add(this.btnSelecionarFoto);
-            this.panel1.Controls.Add(this.dateTimePickerDataCadastro);
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Controls.Add(this.pictureBoxFoto);
-            this.panel1.Controls.Add(this.panel4);
-            this.panel1.Controls.Add(this.panel3);
-            this.panel1.Location = new System.Drawing.Point(3, 2);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(803, 450);
-            this.panel1.TabIndex = 0;
-            // 
             // btnSalvar
             // 
             this.btnSalvar.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnSalvar.Location = new System.Drawing.Point(710, 402);
+            this.btnSalvar.Location = new System.Drawing.Point(715, 413);
             this.btnSalvar.Name = "btnSalvar";
             this.btnSalvar.Size = new System.Drawing.Size(75, 23);
-            this.btnSalvar.TabIndex = 11;
+            this.btnSalvar.TabIndex = 21;
             this.btnSalvar.Text = "Salvar";
             this.btnSalvar.UseVisualStyleBackColor = true;
-            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
             // 
             // btnSelecionarFoto
             // 
             this.btnSelecionarFoto.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnSelecionarFoto.Location = new System.Drawing.Point(604, 402);
+            this.btnSelecionarFoto.Location = new System.Drawing.Point(620, 415);
             this.btnSelecionarFoto.Name = "btnSelecionarFoto";
             this.btnSelecionarFoto.Size = new System.Drawing.Size(75, 23);
-            this.btnSelecionarFoto.TabIndex = 10;
+            this.btnSelecionarFoto.TabIndex = 20;
             this.btnSelecionarFoto.Text = "Selecionar";
             this.btnSelecionarFoto.UseVisualStyleBackColor = true;
-            this.btnSelecionarFoto.Click += new System.EventHandler(this.btnSelecionarFoto_Click);
             // 
             // dateTimePickerDataCadastro
             // 
             this.dateTimePickerDataCadastro.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.dateTimePickerDataCadastro.Location = new System.Drawing.Point(3, 333);
+            this.dateTimePickerDataCadastro.Location = new System.Drawing.Point(8, 343);
             this.dateTimePickerDataCadastro.Name = "dateTimePickerDataCadastro";
             this.dateTimePickerDataCadastro.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePickerDataCadastro.TabIndex = 9;
-            this.dateTimePickerDataCadastro.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            this.dateTimePickerDataCadastro.TabIndex = 19;
             // 
             // label4
             // 
             this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(9, 317);
+            this.label4.Location = new System.Drawing.Point(14, 327);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 8;
+            this.label4.TabIndex = 18;
             this.label4.Text = "label4";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // label3
             // 
             this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(251, 134);
+            this.label3.Location = new System.Drawing.Point(224, 129);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 7;
+            this.label3.TabIndex = 17;
             this.label3.Text = "label3";
             // 
             // label2
             // 
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 118);
+            this.label2.Location = new System.Drawing.Point(14, 109);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 6;
+            this.label2.TabIndex = 16;
             this.label2.Text = "label2";
             // 
             // textBox1
             // 
             this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBox1.Location = new System.Drawing.Point(237, 150);
+            this.textBox1.Location = new System.Drawing.Point(240, 158);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(433, 154);
-            this.textBox1.TabIndex = 5;
+            this.textBox1.TabIndex = 15;
             // 
             // pictureBoxFoto
             // 
             this.pictureBoxFoto.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.pictureBoxFoto.Location = new System.Drawing.Point(9, 134);
+            this.pictureBoxFoto.Location = new System.Drawing.Point(14, 142);
             this.pictureBoxFoto.Name = "pictureBoxFoto";
             this.pictureBoxFoto.Size = new System.Drawing.Size(191, 170);
             this.pictureBoxFoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBoxFoto.TabIndex = 4;
+            this.pictureBoxFoto.TabIndex = 14;
             this.pictureBoxFoto.TabStop = false;
             // 
             // panel4
@@ -150,10 +127,10 @@
             this.panel4.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(74)))), ((int)(((byte)(141)))));
             this.panel4.Controls.Add(this.label1);
-            this.panel4.Location = new System.Drawing.Point(206, 3);
+            this.panel4.Location = new System.Drawing.Point(211, 14);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(581, 75);
-            this.panel4.TabIndex = 3;
+            this.panel4.TabIndex = 13;
             // 
             // label1
             // 
@@ -171,35 +148,49 @@
             this.panel3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.panel3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel3.BackgroundImage")));
             this.panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.panel3.Location = new System.Drawing.Point(3, 3);
+            this.panel3.Location = new System.Drawing.Point(8, 14);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(197, 74);
-            this.panel3.TabIndex = 2;
+            this.panel3.TabIndex = 12;
             // 
-            // CadastroItens
+            // btnExcluir
+            // 
+            this.btnExcluir.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnExcluir.Location = new System.Drawing.Point(529, 415);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 22;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // EdicaoDeItens
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.panel1);
-            this.Name = "CadastroItens";
-            this.Text = "CadastroItens";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnSelecionarFoto);
+            this.Controls.Add(this.dateTimePickerDataCadastro);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBoxFoto);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Name = "EdicaoDeItens";
+            this.Text = "EdicaoDeItens";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFoto)).EndInit();
             this.panel4.ResumeLayout(false);
             this.panel4.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBoxFoto;
         private System.Windows.Forms.Button btnSalvar;
         private System.Windows.Forms.Button btnSelecionarFoto;
         private System.Windows.Forms.DateTimePicker dateTimePickerDataCadastro;
@@ -207,5 +198,10 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBoxFoto;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnExcluir;
     }
 }
